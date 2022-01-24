e05a78a1cd54e6b2f79d0d788e0c930587ee5b4d
Omar HossamEldin <ohossam@couryah.com>
2022-01-24 13:48:05 +0200

    - Update resources. - Update text cell layout.

--- Chat Library/TableViewCells/ReceiverTextTableViewCell.swift
+++ Chat Library/TableViewCells/ReceiverTextTableViewCell.swift
@@ -8,11 +8,9 @@
 import UIKit
 
 class ReceiverTextTableViewCell: TextTableViewCell {
-
-    @IBOutlet weak var polygonImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        polygonImage.transform = CGAffineTransform(scaleX: -1, y: 1)
     }
     
     static func getIdentifier() -> String {
